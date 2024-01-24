cfa0c3ef1cf437ad621be266ad8db7886a3d5f4f
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2024-01-24 10:48:30 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicio2multiploC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escribe un numero de dos digito");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if (numero < 10)
+            {
+                Console.WriteLine("escribe un numero de dos digitos");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
